79c62ac8a14ded0054b729c1c6f8fb8faed1f18d
Marko Dujović <dujovicm@outlook.com>
2023-04-04 16:33:18 +0200

    Initial commit

---
+++ WebServer/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WebServer.Models
+{
+    public class User
+    {
+        [DisallowNull]
+        private string username, email, password;
+        
+        public string Username { get => username; set => username = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}

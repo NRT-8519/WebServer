3a68a5789e60cc2c647ad43575dc983d34122c99
Marko Dujović <dujovicm@outlook.com>
2023-04-16 20:21:12 +0200

    Bump

--- WebServer/Auth/Roles.cs
+++ WebServer/Auth/Roles.cs
@@ -1,6 +1,9 @@
 ﻿namespace WebServer.Auth
 {
-    public class Roles
+    public class Role
     {
+        public static string ROLE_USER = "User";
+        public static string ROLE_DOCTOR = "Doctor";
+        public static string ROLE_ADMIN = "Administrator";
     }
 }
--- WebServer/Models/UserModel.cs
+++ WebServer/Models/UserModel.cs
@@ -1,14 +1,69 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using WebServer.Auth;
 
 namespace WebServer.Models
 {
-    public class User
+    public class UserModel
     {
         [DisallowNull]
         private string username, email, password;
-        
+
+        [DisallowNull]
+        private bool isDisabled, isExpired;
+
+        [DisallowNull]
+        private DateTime passwordExpiryDate;
+
+        private List<string> roles = new();
+
         public string Username { get => username; set => username = value; }
         public string Email { get => email; set => email = value; }
         public string Password { get => password; set => password = value; }
+
+        public bool IsDisabled { get => isDisabled; set => isDisabled = value; }
+        public bool IsExpired { get => isExpired; set => isExpired = value; }
+
+        public DateTime PasswordExpiryDate { get => passwordExpiryDate; set => passwordExpiryDate = value; }
+
+        public List<string> Roles { get => roles; set => roles = value; }
+        
+        /// <summary>
+        ///    Constructor that sets the default initial values
+        /// </summary>
+        private UserModel()
+        {
+            this.IsDisabled = false;
+            this.IsExpired = false;
+            this.PasswordExpiryDate = DateTime.Now.AddDays(180);
+            this.Roles.Add(Role.ROLE_USER);
+        }
+
+        public UserModel(string username, string email, string password): this()
+        {
+            this.Username = username;
+            this.Email = email;
+            this.Password = password;
+        }
+
+        public UserModel(string username, string email, string password, bool isDisabled, bool isExpired) : this(username, email, password)
+        {
+            this.IsDisabled = isDisabled;
+            this.IsExpired = isExpired;
+
+            if (isExpired)
+            {
+                this.PasswordExpiryDate = DateTime.Now;
+            }
+        }
+
+        public UserModel(string username, string email, string password, bool isDisabled, bool isExpired, DateTime passwordExpiryDate) : this(username, email, password, isDisabled, isExpired)
+        {
+            this.PasswordExpiryDate = passwordExpiryDate;
+        }
+
+        public UserModel(string username, string email, string password, bool isDisabled, bool isExpired, DateTime passwordExpiryDate, List<string> roles) : this(username, email, password, isDisabled, isExpired, passwordExpiryDate)
+        {
+            this.Roles = roles;
+        }
     }
 }

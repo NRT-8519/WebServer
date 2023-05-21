d9c0d7ef366f58ca9118b1e7eeec4e364be895a1
Marko Dujović <dujovicm@outlook.com>
2023-05-21 18:15:15 +0200

    Controller abstract class

---
+++ WebServer/Controllers/Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebServer.Models;
+using WebServer.Services;
+
+namespace WebServer.Controllers
+{
+    public abstract class Controller<T> : ControllerBase
+    {
+        public Controller() { }
+
+        public abstract Task<ActionResult<IEnumerable<T>>> GetAll(); 
+        public abstract Task<ActionResult<T>> GetById(int id);
+        public abstract Task<ActionResult<T>> GetByUUID(Guid UUID);
+        public abstract Task<ActionResult<T>> Add(Company company);
+        public abstract Task<ActionResult<T>> Edit(Company company);
+        public abstract Task<ActionResult<T>> Remove(Company company);
+        public abstract Task<ActionResult<T>> RemoveById(int id);
+        public abstract Task<ActionResult<T>> RemoveByUUID(Guid UUID);
+    }
+}

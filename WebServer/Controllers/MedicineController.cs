4b77e55764a5bf6d458625ab85488bb157c9a70f
Marko Dujović <dujovicm@outlook.com>
2023-05-25 09:06:05 +0000

    REST Definitions

--- WebServer/Controllers/MedicineController.cs
+++ WebServer/Controllers/MedicineController.cs
@@ -1,45 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using WebServer.Models.MedicineData;
 
 namespace WebServer.Controllers
 {
+    [Authorize]
+    [Route("api/medicine")]
+    [ApiController]
     public class MedicineController : Controller<Medicine>
     {
-        public override async Task<ActionResult<Medicine>> Add(Medicine entity)
+
+        [HttpPost("add")]
+        public override async Task<ActionResult<Medicine>> Add([FromBody] Medicine entity)
         {
             throw new NotImplementedException();
         }
 
-        public override async Task<ActionResult<Medicine>> Edit(Medicine entity)
+        [HttpPut("edit")]
+        public override async Task<ActionResult<Medicine>> Edit([FromBody] Medicine entity)
         {
             throw new NotImplementedException();
         }
 
+        [HttpGet("all")]
         public override async Task<ActionResult<IEnumerable<Medicine>>> GetAll()
         {
             throw new NotImplementedException();
         }
-
+        
+        [HttpGet("{id}")]
         public override async Task<ActionResult<Medicine>> GetById(int id)
         {
             throw new NotImplementedException();
         }
 
+        [HttpGet("{UUID}")]
         public override async Task<ActionResult<Medicine>> GetByUUID(Guid UUID)
         {
             throw new NotImplementedException();
         }
-
+        
+        [HttpDelete("remove")]
         public override async Task<ActionResult<Medicine>> Remove(Medicine entity)
         {
             throw new NotImplementedException();
         }
 
+        [HttpDelete("remove/{id}")]
         public override async Task<ActionResult<Medicine>> RemoveById(int id)
         {
             throw new NotImplementedException();
         }
 
+        [HttpDelete("remove/{UUID}")]
         public override async Task<ActionResult<Medicine>> RemoveByUUID(Guid UUID)
         {
             throw new NotImplementedException();

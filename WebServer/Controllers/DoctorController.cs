44c5390f2a983d3eb02e299e59b6ef2622b59a65
Gamma1772 <dujovicm@outlook.com>
2023-07-30 12:50:47 +0200

    Add separate doctor and patient controllers

---
+++ WebServer/Controllers/DoctorController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebServer.Authentication;
+using WebServer.Models.ClinicData.Entities;
+using WebServer.Models.UserData;
+using WebServer.Services;
+
+namespace WebServer.Controllers
+{
+    [Authorize]
+    [Route("api/users/doctors")]
+    [ApiController]
+    public class DoctorController : Controller<Doctor>
+    {
+        public DoctorController(ILogger<DoctorController> logger, IDbService<Doctor> service) : base(logger, service)
+        {
+        }
+
+        [HttpGet("all")]
+        [Authorize(Roles = "ADMINISTRATOR,DOCTOR,PATIENT,USER")]
+        public override async Task<IActionResult> GetAll()
+        {
+            var result = await ((DoctorService)service).FindAll();
+            if (result.Any())
+            {
+                logger.LogInformation("Fetched all doctors.");
+                return Ok(result);
+            }
+            else
+            {
+                logger.LogInformation("Doctors database empty.");
+                return NoContent();
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = "ADMINISTRATOR")]
+        public override async Task<IActionResult> GetById(int id)
+        {
+            var doctor = await ((DoctorService)service).FindById(id);
+            if (doctor != default)
+            {
+                return Ok(doctor);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{UUID}")]
+        [Authorize(Roles = "ADMINISTRATOR,DOCTOR,PATIENT")]
+        public override async Task<IActionResult> GetByUUID(Guid UUID)
+        {
+            var doctor = await ((DoctorService)service).FindByUUID(UUID);
+            if (doctor != default)
+            {
+                return Ok(doctor);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("add")]
+        [Authorize(Roles = "ADMINISTRATOR")]
+        public override async Task<IActionResult> Add([FromBody] Doctor model)
+        {
+            var user = await ((DoctorService)service).Insert(model);
+
+            if (user != 0)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("edit")]
+        [Authorize(Roles = "ADMINISTRATOR,DOCTOR")]
+        public override async Task<IActionResult> Edit([FromBody] Doctor model)
+        {
+            var user = await ((DoctorService)service).Update(model);
+
+            if (user != 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("remove")]
+        [Obsolete]
+        public override async Task<IActionResult> Remove(Doctor entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("remove/{id}")]
+        [Obsolete]
+        public override async Task<IActionResult> RemoveById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("remove/{UUID}")]
+        [Obsolete]
+        public override async Task<IActionResult> RemoveByUUID(Guid UUID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

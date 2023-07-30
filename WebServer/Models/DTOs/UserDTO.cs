1f9e18efcdcfadd167ebd8f95c6b0486c1d5e711
Gamma1772 <dujovicm@outlook.com>
2023-07-30 12:51:24 +0200

    User DTO

---
+++ WebServer/Models/DTOs/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace WebServer.Models.DTOs
+{
+    public class UserDTO
+    {
+        public Guid UUID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string SSN { get; set; }
+        public char Gender { get; set; }
+        public List<string> Emails { get; set; }
+        public List<string> PhoneNumbers { get; set; }
+        public string Role { get; set; }
+        public bool IsDisabled { get; set; }
+        public bool IsExpired { get; set; }
+        public DateTime PasswordExpiry { get; set; }
+    }
+}
